875141159f8aa8eaf3cbfdcc608aa4a40e11b8ae
Liv_Alles <38350@ma-web.nl>
2026-01-14 12:17:55 +0100

    ddoge

--- cult of the lamb/Assets/Scripts/Movement/Dodge.cs
+++ cult of the lamb/Assets/Scripts/Movement/Dodge.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 public class Dodge : MonoBehaviour
 {
     Rigidbody _rb;
-    bool _dodging = false;
     [SerializeField] float _rollSpeed;
+    [SerializeField] float _rollTime;
+    bool _dodging = true;
+
 
     [SerializeField] LayerMask _playerLayer;
     [SerializeField] LayerMask _enemyLayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("Dodge"))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _dodging)
         {
-
+           StartCoroutine(Roll());
         }
+           
     }
     private IEnumerator Roll()
     {
-        _dodging = true;
+        _dodging = false;
         Physics.IgnoreLayerCollision(_playerLayer, _enemyLayer, _dodging);
+        _rb.AddForce(_rollSpeed * _rb.transform.right, ForceMode.Impulse);
+        
+        yield return new WaitForSeconds(_rollTime);
+        _dodging = true;
     }
 }
